ee97fb9056d21c60983c4d32bf9ec2ccb272e09e
ahmedshehata777 <ahmedshe771177@gmail.com>
2024-02-28 22:04:43 +0200

    user profile he can enter and edite his data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import './App.css';
-
+import "./App.css";
+import React from "react";
 import Wishlist from "./pages/wishlist/Wishlist";
 import MyNavbar from "./component/NavBar/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HotelsFilter from "./pages/HotelsFilter";
+import UserProfilePage from "./pages/user_profile/user_profile";
 
-import HotelsFilter from './pages/HotelsFilter';
+
 function App() {
-
   return (
     <div className="App">
-
       <BrowserRouter>
-      <MyNavbar/>
-      <Switch>
-      
-        <Route exact path={"/HotelsFilter"} component={HotelsFilter}/>
-        <Route exact path={"/Wishlist"} component={Wishlist} />
-
-      </Switch>
-    </BrowserRouter>
+        <MyNavbar />
+        <Switch>
+          <Route exact path={"/HotelsFilter"} component={HotelsFilter} />
+          <Route exact path={"/Wishlist"} component={Wishlist} />
+          <Route exact path={"/userprofile"} component={UserProfilePage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/NavBar/Navbar.js
+++ src/component/NavBar/Navbar.js
@@ -1,97 +1,112 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
-import './nav.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import "./nav.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 function MyNavbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    Navbar
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="HotelsFilter">
-                                List Hotels
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="Wishlist">
-                                WishList
-                            </Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdown"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <hr className="dropdown-divider" />
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link disabled"
-                                href="#"
-                                tabIndex={-1}
-                                aria-disabled="true"
-                            >
-                                Disabled
-                            </a>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button className="btn btn-outline-success" type="submit">
-                            Search
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="HotelsFilter"
+              >
+                List Hotels
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="Wishlist">
+                WishList
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link disabled"
+                href="#"
+                tabIndex={-1}
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+          <div className="d-flex m-2">
+            <Link to="/userprofile">
+              <AccountCircleIcon fontSize="large"></AccountCircleIcon>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default MyNavbar;
