268dca186f79382584d763e6c27f7949f03e2ff6
mahmoud-badri <eng.mahmoud.badrii@gmail.com>
2024-03-01 22:28:17 +0200

    save updated

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import "./App.css";
 import React from "react";
 import Wishlist from "./pages/wishlist/Wishlist";
 import MyNavbar from "./component/NavBar/Navbar";
-
 import Register from "./pages/Authorization/Register";
 import Login from "./pages/Authorization/Login";
-function App() {
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import HotelsFilter from "./pages/HotelsFilter";
 import UserProfilePage from "./pages/user_profile/user_profile";
-
-// import HotelsFilter from "./pages/HotelsFilter";
 import Home from "./pages/Homepage/home_page";
 import ParticlesComponent from "./component/particles/particles";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -24,9 +18,10 @@ function App() {
 
 
         <MyNavbar />
-         <ParticlesComponent id="particles" />
+        <ParticlesComponent id="particles" />
         <Switch>
             <Route exact path={"/ListHotel"} component={ListHotel}/>
+            <Route exact path={"/HotelsFilter"} component={HotelsFilter}/>
             <Route exact path={"/Wishlist"} component={Wishlist} />
             <Route exact path={"/Register"} component={Register} />
             <Route exact path={"/Login"} component={Login} />
