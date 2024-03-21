cc4156ba51e6e267e1e6bde4ddf5449c811902ba
ahmedshehata777 <ahmedshe771177@gmail.com>
2024-03-21 19:00:16 +0200

    lastmodefay

--- src/pages/hotel-details/HotelDetails.js
+++ src/pages/hotel-details/HotelDetails.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import HotelDetailHeader from '../../component/hotel_detail_header/HotelDetailHeader'
-import HotelDetailBody from '../../component/hotel_detail_body/HotelDetailBody'
+import React from "react";
+import HotelDetailHeader from "../../component/hotel_detail_header/HotelDetailHeader";
+import HotelDetailBody from "../../component/hotel_detail_body/HotelDetailBody";
 
-
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 export default function HotelDetails() {
+  const { state } = useLocation();
   return (
     <div>
-      <HotelDetailHeader />
-      <HotelDetailBody />
+      <HotelDetailHeader data={state} />
+      <HotelDetailBody data={state} />
     </div>
-  )
+  );
 }
+
