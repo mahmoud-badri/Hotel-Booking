7c25fa5e696f7fecc600d7e91c0a7b517e588786
ahmedshehata777 <ahmedshe771177@gmail.com>
2024-03-23 23:59:16 +0200

    fixing git conflict

---
+++ src/component/add_room_form/AddRoomForm.js
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addHotel } from '../../Redux/HotelAction';
+import './addRoomForm.css'
+const AddRoomForm = () => {
+    
+
+    const [formData, setFormData] = useState({
+        name: '',
+        address: '',
+        description: '',
+        prices: 0,
+        governorate: '',
+        image: null,
+    });
+
+    const handleInputChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleImageChange = (e) => {
+        setFormData({ ...formData, image: e.target.files[0] });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addHotel(formData);
+        window.location.reload();
+    };
+
+    return (
+        <form className="hotel-form" onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    Name:
+                    <input
+                        type='text'
+                        name='name'
+                        value={formData.name}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Address:
+                    <textarea
+                        name='address'
+                        value={formData.address}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Description:
+                    <textarea
+                        name='description'
+                        value={formData.description}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <br />
+               
+                <label>
+                    Prices:
+                    <input
+                        type='number'
+                        name='prices'
+                        value={formData.prices}
+                        onChange={handleInputChange}
+                        min={0}
+                        step={0.01}
+                    />
+                </label>
+                <br />
+               
+                <label>
+                    Governorate:
+                    <input
+                        type='text'
+                        name='governorate'
+                        value={formData.governorate}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Image:
+                    <input
+                        type='file'
+                        name='image'
+                        onChange={handleImageChange}
+                    />
+                </label>
+                <label>
+                    Single Room Price:
+                    <input
+                        type='number'
+                        name='single_room'
+                        value={formData.single_room}
+                        onChange={handleInputChange}
+                        min={0}
+                        step={0.01}
+                    />
+                </label>
+                <br />
+                <label>
+                    Suite Price:
+                    <input
+                        type='number'
+                        name='suite'
+                        value={formData.suite}
+                        onChange={handleInputChange}
+                        min={0}
+                        step={0.01}
+                    />
+                </label>
+                <br />
+                <label>
+                    Family Room Price:
+                    <input
+                        type='number'
+                        name='family_room'
+                        value={formData.family_room}
+                        onChange={handleInputChange}
+                        min={0}
+                        step={0.01}
+                    />
+                </label>
+                <br />
+                <button
+                    className='btn btn-danger w-50 fw-bold'
+                    type='submit'
+                    style={{ transition: 'background-color 0.3s, color 0.3s' }}
+                    onMouseOver={(e) => e.target.classList.add('btn-hover')}
+                    onMouseOut={(e) => e.target.classList.remove('btn-hover')}
+                >
+                    Add Hotel
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default AddRoomForm;
