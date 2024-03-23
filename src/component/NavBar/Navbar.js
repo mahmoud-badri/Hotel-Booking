d0acf06118439590ebf18617dd3c5d13564528e6
mostafa <mostufazx@gmail.com>
2024-03-23 13:40:48 +0200

    ubdate_nav

--- src/component/NavBar/Navbar.js
+++ src/component/NavBar/Navbar.js
@@ -1,6 +1,3 @@
-
-
-// export default MyNavbar;
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
@@ -19,165 +16,92 @@ import { AuthContext } from "../../Context/AuthContext";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function MyNavbar() {
-const { contextLoggedIn, setContextLoggenIn } = useContext(LoggedInContext);
-const authContext = useContext(AuthContext);
-const history = useHistory();
-const isLoggedIn = authContext.isLoggedIn;
-const currentUser = authContext.currentUser;
+  const { contextLoggedIn, setContextLoggenIn } = useContext(LoggedInContext);
+  const authContext = useContext(AuthContext);
+  const history = useHistory();
+  const isLoggedIn = authContext.isLoggedIn;
+  const currentUser = authContext.currentUser;
 
-useEffect(() => {
+  useEffect(() => {
     history.push("/");
-}, [isLoggedIn, currentUser]);
+  }, [isLoggedIn, currentUser]);
 
-const logOut = () => {
-localStorage.removeItem("loginUser");
-setContextLoggenIn("");
-authContext.logout();
-};
+  const logOut = () => {
+    localStorage.removeItem("loginUser");
+    setContextLoggenIn("");
+    authContext.logout();
+  };
   
-var user = localStorage.getItem("user")
- user = JSON.parse(user)
+  var user = localStorage.getItem("user")
+  user = JSON.parse(user)
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
-          <h3>
-            {" "}
-            ITI <sub>bnb</sub>{" "}
-          </h3>
-        </Link>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link
-                className="nav-link active"
-                aria-current="page"
-                to="HotelsFilter">
-                List Hotels
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link active" to="Wishlist">
-                WishList
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link
-                className="nav-link active"
-                aria-current="page"
-                to="AddHotelForm"
-              >
-                Add Hotel
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
+    <>
+      {/* First Navbar */}
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Link to="/" className="navbar-brand">
+            <h3>
+              {" "}
+              ITI <sub>bnb</sub>{" "}
+            </h3>
+          </Link>
+          <Form className="d-flex mx-auto">
+            <FormControl
               type="search"
               placeholder="Search"
+              className="mr-2"
               aria-label="Search"
             />
-
-{!isLoggedIn ? (
-    <>
-       
-       <div className="d-flex m-2">
-            <Link className="nav-link active" aria-current="page" to="Register">
-             Register
-            </Link>
-          </div>
-       
-        
-    </>
- ) :(
-    <>
-    <div className="d-flex m-2">
-    {`Hello  ${user.name}`}
-    </div>
-    
-    <div className="nav-item">
-    <Link to="Dashboard" className="nav-link">
-    Dashboard
-    </Link>
-    </div>
-    
-    <div className="d-flex m-2">
-        <Link
-        className="nav-link active"
-        aria-current="page"
-        to="/"
-        onClick={logOut}
-        >
-        log out
-        </Link>
-        </div>
-        </>
- )
-/* <li className="nav-item">
-<Link className="nav-link active" to="Dashboard">
-Dashboard
-</Link>
-</li> */}
-
-
-
-
-
-
-            <button className="btn custom-search-btn" type="submit">
-              Search
-            </button>
-          </form>
-
-          
-
-          {/* <div className="d-flex m-2">
-            {`${contextLoggedIn ? "Hello " + contextLoggedIn.username : ""}`}
-          </div>
-
-          {contextLoggedIn && (
-            <div className="nav-item">
-              <Link to="Dashboard" className="nav-link">
-                Dashboard
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Nav>
+            {isLoggedIn && (
+              <Link to="/userprofile">
+                <AccountCircleIcon fontSize="large"></AccountCircleIcon>
               </Link>
-            </div>
-          )} */}
+            )}
+          </Nav>
+        </Container>
+      </Navbar>
 
-
-          {/* <div className="d-flex m-2">
-
-          <div className="d-flex m-2">
-            <Link
-              className="nav-link active"
-              aria-current="page"
-              to="/"
-              onClick={logOut}>
-              {`${contextLoggedIn ? "Log out" : ""}`}
-            </Link>
-          </div> */}
-
-          <div className="d-flex m-2">
-            <Link to="/userprofile">
-              <AccountCircleIcon fontSize="large"></AccountCircleIcon>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+      {/* Second Navbar */}
+      <Navbar bg="light" expand="lg" className="mb-4">
+        <Container className="justify-content-center">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/HotelsFilter">
+                List Hotels
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Wishlist">
+                WishList
+              </Nav.Link>
+              <Nav.Link as={Link} to="/AddHotelForm">
+                Add Hotel
+              </Nav.Link>
+              {!isLoggedIn ? (
+                <Nav.Link as={Link} to="/Register">
+                  Register
+                </Nav.Link>
+              ) : (
+                <>
+                  <div className="d-flex m-2">
+                    {`Hello  ${user.name}`}
+                  </div>
+                  <Nav.Link as={Link} to="/Dashboard">
+                    Dashboard
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/" onClick={logOut}>
+                    log out
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
